0c5ec980b09c56625c395faaf063457c08b2f4ca
kolit0 <kolo03057@gmail.com>
2024-07-16 22:29:23 -0400

    sidebar open

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -19,7 +19,7 @@ function Sidebar (){
   const Menus = [
     { title: t("sidebar.home"), icon: <FiHome />, href: "../Home"},
     { title: t("sidebar.pages"), 
-      icon: <MdOutlineWebAsset />,
+      icon: <MdOutlineWebAsset onClick={() => setOpen(!open) & setSubmenuOpen2(!submenuopen2)} />,
       submenu2: true,
       subemnuItems2: [
         {title: "Grupo Nouma", href: "https://www.gruponouma.com"},
@@ -38,7 +38,7 @@ function Sidebar (){
         <div className="flex">
             <div className={`bg-[#0079FF] h-screen ${open ? "md:w-72 w-60 md:opacity-100 opacity-95" : "w-20"} duration-500 absolute z-10`}
             >
-                <BsArrowLeftShort className={`bg-white text-[#0079FF] text-3xl rounded-full absolute -right-10 top-9 border border-blue-500 cursor-pointer -mt-2 ${!open && "rotate-180"}`} onClick={() => setOpen(!open)}/>
+                <BsArrowLeftShort className={`bg-white text-[#0079FF] text-3xl rounded-full absolute -right-10 top-9 border border-blue-500 cursor-pointer -mt-2 ${!open && "rotate-180"}`} onClick={() => setOpen(!open) & setSubmenuOpen2(!submenuopen2)}/>
 
                 <div className={` bg-white ${!open ? "w-[5rem] items-center inline-flex  h-20 px-4": "md:px-16 pl-8"} ${!open ? " py-4 pb-0 duration-500": " flex justify-center"}`}>
                   {!open ? <motion.img id="NoumaIco" src={noumaIco} className={`size-10 cursor-pointer block float-left mr-2 duration-500`}
@@ -52,8 +52,7 @@ function Sidebar (){
                 {Menus.map((menu, index) =>(
                     <>
                         <li key={index} className={`text-gray-300 text-xl flex items-center gap-x-4 cursor-pointer p-2 hover:bg-[#00DFA2] hover:text-black ${menu.spacing ? "mt-9" : "mt-2"} mt-2  border-b-2`} >
-                            <a className="text-2xl block float-left" href={menu.href}
-                            onClick={() => setOpen(!open)}>
+                            <a className="text-2xl block float-left" href={menu.href}>
                             {menu.icon ? menu.icon : <MdSpaceDashboard/>}
                             
                             </a>
